4f4cbb28ae0b5f9997401305259f8b611049bd98
André Pereira Fonseca <afonseca@fatorseguradora.com.br>
2025-10-27 19:45:13 -0300

    Conclusão do desafio

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,13 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(connString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
